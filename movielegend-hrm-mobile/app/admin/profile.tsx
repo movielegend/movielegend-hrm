51956694b97a1c4074b06739b106bebdff20f636
Thanh Binh <binhpthe173173@fpt.edu.vn>
2026-07-08 09:42:20 +0700

    Add BottomNavBar to admin profile screen

--- movielegend-hrm-mobile/app/admin/profile.tsx
+++ movielegend-hrm-mobile/app/admin/profile.tsx
@@ -1,5 +1,12 @@
+import { View } from 'react-native';
 import { EmployeeProfileScreen } from '../../src/features/employees/EmployeeProfileScreen';
+import { BottomNavBar } from '../../src/components/BottomNavBar';
 
 export default function EmployeeProfileRoute() {
-  return <EmployeeProfileScreen />;
+  return (
+    <View style={{ flex: 1 }}>
+      <EmployeeProfileScreen />
+      <BottomNavBar activeTab="profile" />
+    </View>
+  );
 }
