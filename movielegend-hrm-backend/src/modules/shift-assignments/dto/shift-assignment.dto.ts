623c822ce8f3eec2b6f075ddb472450d6a962e4e
movielegend <137597832+movielegend@users.noreply.github.com>
2026-07-22 15:25:36 +0700

    fix: import IsArray in shift-assignment.dto

--- movielegend-hrm-backend/src/modules/shift-assignments/dto/shift-assignment.dto.ts
+++ movielegend-hrm-backend/src/modules/shift-assignments/dto/shift-assignment.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsUUID } from 'class-validator';
+import { IsArray, IsDateString, IsUUID } from 'class-validator';
 
 export class AssignShiftDto {
   @ApiProperty()
