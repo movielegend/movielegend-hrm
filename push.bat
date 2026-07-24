30f2b7b085597f10f5afc1b83bf89c37ff2bf466
movielegend <137597832+movielegend@users.noreply.github.com>
2026-07-24 11:27:52 +0700

    Cấu hình build EAS xuất ra file APK thay vì AAB

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 cd d:\MovieLegend
 git add .
-git commit -m "Cập nhật logic thông báo và hiển thị tên người dùng trong Bảng tin"
+git commit -m "Cấu hình build EAS xuất ra file APK thay vì AAB"
 git pull --rebase
 git push
