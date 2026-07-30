30506f3539a4f424a3ccd1bfd3ed33717dde084c
Thanh Binh <binhpthe173173@fpt.edu.vn>
2026-07-30 17:46:08 +0700

    fix: add RealtimeModule to tasks module

--- movielegend-hrm-backend/src/modules/tasks/tasks.module.ts
+++ movielegend-hrm-backend/src/modules/tasks/tasks.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { Phase2PolicyModule } from '../phase2-policy/phase2-policy.module';
+import { RealtimeModule } from '../realtime/realtime.module';
 import { StorageModule } from '../storage/storage.module';
 import { TaskAssignmentsController, TaskExtensionsController, TasksController } from './tasks.controller';
 import { TaskPolicyService } from './task-policy.service';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [DatabaseModule, Phase2PolicyModule, NotificationsModule, StorageModule],
+  imports: [DatabaseModule, Phase2PolicyModule, NotificationsModule, StorageModule, RealtimeModule],
   controllers: [TasksController, TaskAssignmentsController, TaskExtensionsController],
   providers: [TasksService, TaskPolicyService],
   exports: [TasksService, TaskPolicyService],
