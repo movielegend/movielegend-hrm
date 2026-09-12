9e1a6196ae1a6c878cf27fdb4a3ab022dd587971
movielegend <137597832+movielegend@users.noreply.github.com>
2026-09-12 15:54:48 +0700

    fix(mobile): configure Google Maps API key fallback and set developmentClient false on standalone builds

--- movielegend-hrm-mobile/app.config.js
+++ movielegend-hrm-mobile/app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => {
       config: {
         ...config.android?.config,
         googleMaps: {
-          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || 'AIzaSyCsSPp2vj3uEo4wgp2wwkj051CLr04NeFE'
         }
       }
     }
